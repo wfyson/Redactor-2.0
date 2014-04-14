c16f405d495ccd96c77947581ae931c1a3c3ed02
rwf1v07 <rwf1v07@UOS-205394.soton.ac.uk>
2014-04-14 17:03:35 +0100

    Initial Commit. Start on a homepage, with a progress bar that shows a file being uploaded and php scripts to slice up large files and upload them.

---
+++ public_html/php/merge.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+
+$id = session_id();
+
+$path = $id . '/';
+
+if(!isset($_REQUEST['name'])) throw new Exception('Name required');
+if(!preg_match('/^[-a-z0-9_][-a-z0-9_.]*$/i', $_REQUEST['name'])) throw new Exception('Name error');
+
+if(!isset($_REQUEST['index'])) throw new Exception('Index required');
+if(!preg_match('/^[0-9]+$/', $_REQUEST['index'])) throw new Exception('Index error');
+
+$target = $path . $_REQUEST['name'];
+$dst = fopen($target, 'wb');
+
+for($i = 0; $i < $_REQUEST['index']; $i++) {
+    $slice = $target . '-' . $i;
+    $src = fopen($slice, 'rb');
+    stream_copy_to_stream($src, $dst);
+    fclose($src);
+    unlink($slice);
+}
+
+fclose($dst);
+
+echo $id;
+?>
---
+++ public_html/php/writer.php
@@ -0,0 +1,34 @@
+<?php
+
+session_start();
+
+$id = session_id();
+
+$path = $id . '/';
+
+if (!file_exists($path)) {
+    mkdir($path, 0777, true);
+}
+
+if (!isset($_REQUEST['name']))
+    throw new Exception('Name required');
+if (!preg_match('/^[-a-z0-9_][-a-z0-9_.]*$/i', $_REQUEST['name']))
+    throw new Exception('Name error');
+
+if (!isset($_REQUEST['index']))
+    throw new Exception('Index required');
+if (!preg_match('/^[0-9]+$/', $_REQUEST['index']))
+    throw new Exception('Index error');
+
+if (!isset($_FILES['file']))
+    throw new Exception('Upload required');
+if ($_FILES['file']['error'] != 0)
+    throw new Exception('Upload error');
+
+$target = $path . $_REQUEST['name'] . '-' . $_REQUEST['index'];
+
+move_uploaded_file($_FILES['file']['tmp_name'], $target);
+
+// Might execute too quickly.
+sleep(1);
+?>
