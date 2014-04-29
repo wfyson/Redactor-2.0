4e10fe0a31a0bae3bbea67c20a35198630c5687d
rwf1v07 <rwf1v07@UOS-205394.soton.ac.uk>
2014-04-29 16:05:23 +0100

    Captions are now generated and added to a slide's XML when an image is replaced.

--- public_html/php/openXmlWriter.php
+++ public_html/php/openXmlWriter.php
@@ -8,6 +8,9 @@
  * Good overview of reading and creating zips here: http://devzone.zend.com/985/dynamically-creating-compressed-zip-archives-with-php/
  */
 
+include 'ChromePhp.php';
+//example for logging: ChromePhp::log('Hello console!');
+
 /*
  * An interface for defining a writer for any file inputs
  */
@@ -29,12 +32,12 @@ abstract class OpenXmlWriter
     protected $changes;
     protected $zipArchive;
     
-    public function __construct($document, $changes=null)
-    {
-
+    public function __construct($document=null, $changes=null)
+    {        
         $id = session_id();
         $this->document = $document;
-        $this->file = $file = $this->document->getFilepath();        
+        $this->file = $document;
+        //$this->file = $file = $this->document->getFilepath();        
         $this->changes = $changes;     
         
         //make a copy of the original file so that we can alter it and send it back with a new name
@@ -53,7 +56,7 @@ abstract class OpenXmlWriter
         
         ChromePhp::log("ready to start writing!!!");
         
-        $this->enactReplaceRedaction(null);
+        $this->enactReplaceRedaction($redaction);
         
         /*
          * need to iterate through the changes, but if each change leaves the
@@ -117,8 +120,8 @@ class PowerPointWriter extends OpenXmlWriter implements DocumentWriter
                 
         //and then captions where appropriate
         
-        //test case where image name = "image1.jpeg"
-        $slideRels = $this->document->getImageRels("image1.jpeg");
+        //test case where image name = "image1.jpeg" but image name would come from the redaction object in reality
+        //$slideRels = $this->document->getImageRels("image3.jpeg");
         
         //read through the slide files and see if the slide no corresponds with a key in the slideRels array
         $this->zip = zip_open($this->file);        
@@ -126,7 +129,19 @@ class PowerPointWriter extends OpenXmlWriter implements DocumentWriter
         while ($zipEntry != false)
         {                       
             $entryName = zip_entry_name($zipEntry);
-            
+            if (strpos($entryName, 'ppt/slides/slide') !== FALSE)
+            {
+                //get the slide number
+                $slideFile = basename($entryName);
+                $slideNo = substr($slideFile, 0, strpos($slideFile, '.'));
+                $no = substr($slideNo, 5);   
+                
+                //if (array_key_exists($no, $slideRels))
+                //{
+                    ChromePhp::log("reading slide..." . $no);
+                    $this->writeCaption($zipEntry, $slideRels[$no], "test caption");
+                //}
+            }
             
             
             
@@ -161,10 +176,174 @@ class PowerPointWriter extends OpenXmlWriter implements DocumentWriter
     /*
      * Add a caption to slide to attribute an image
      */    
-    public function writeCaption($xml, $x, $y, $caption)
+    public function writeCaption($zipEntry, $slideRels, $caption)
     {
+        ChromePhp::log("caption writing!!!");
         
-    }   
+        
+        //read the xml        
+        $slide = zip_entry_read($zipEntry, zip_entry_filesize($zipEntry));
+        $xml = simplexml_load_string($slide);      
+        
+        //get the maximum value for an id
+        $maxId = 0;
+        $ids = $xml->xpath('//p:cNvPr/@id');
+        foreach($ids as $id)
+        {            
+            if((int)$id > $maxId)
+            {
+                $maxId = (int)$id;
+            }
+        }
+        
+        //assume a relId of rId2 (which would normally be acquired via $slideRels->relId;
+        $relId = "rId2";
+        
+        //or doing it with a DOM
+        $doc = new DOMDocument();
+        $doc->loadXML($slide);
+              
+        $xpath = new DOMXPath($doc);
+        $treeQuery = '//p:spTree';
+        $tree = $xpath->query($treeQuery)->item(0);
+        $picQuery = '//p:pic';
+        $pics = $xpath->query($picQuery);
+        foreach($pics as $pic)
+        {          
+            $blipQuery = 'p:blipFill/a:blip/@r:embed';
+            $blip = $xpath->query($blipQuery, $pic)->item(0);
+            if($blip->value == $relId)
+            {
+                $maxId++;
+                
+                //get the position information
+                $offQuery = 'p:spPr/a:xfrm/a:off';                
+                $off = $xpath->query($offQuery, $pic)->item(0);
+                $x = $off->getAttribute('x');
+                $y = $off->getAttribute('y');
+                
+                $extQuery = 'p:spPr/a:xfrm/a:ext';
+                $ext = $xpath->query($extQuery, $pic)->item(0);
+                $cx = $ext->getAttribute('cx');
+                $cy = $ext->getAttribute('cy');
+                
+                //create the text box with caption
+                $sp = $this->createCaption($doc, $maxId, $x, $y, $cx, $cy, $caption);
+                
+                //get sibling
+                $siblingQuery = 'following-sibling::*[1]';
+                $siblings = $xpath->query($siblingQuery, $pic);
+
+                if ($siblings->length > 0)
+                {
+                    $sibling = $siblings->item(0);
+                    $tree->insertBefore($sp, $sibling);
+                }
+                else
+                {
+                    $tree->appendChild($sp);
+                }
+                ChromePhp::log($doc->saveXML());     
+            }
+        }   
+    }
+    
+    public function createCaption($doc, $id, $x, $y, $cx, $cy, $caption)
+    {
+        $sp = $doc->createElement('p:sp');
+        
+        $nvSpPr = $doc->createElement('p:nvSpPr');
+        
+        $cNvPr = $doc->createElement('p:cNvPr');
+        $cNvPr->setAttribute('id', $id);
+        $cNvPr->setAttribute('name', "TextBox");
+        
+        $cNvSpPr = $doc->createElement("p:cNvSpPr");
+        $cNvSpPr->setAttribute("txBox", "1");
+    
+        $nvPr = $doc->createElement("p:nvPr");
+
+        $spPr = $doc->createElement("p:spPr");
+
+        $xfrm = $doc->createElement("a:xfrm");
+
+        $off = $doc->createElement("a:off");
+        $off->setAttribute('x', $x);
+        $off->setAttribute('y', $y);
+
+        $ext = $doc->createElement("a:ext");
+        $ext->setAttribute('cx', $cx);
+        $ext->setAttribute('cy', $cy);
+
+        $prstGeom = $doc->createElement("a:prstGeom");
+        $prstGeom->setAttribute('prst', "rect");
+
+        $avLst = $doc->createElement("a:avLst");
+
+        $noFill = $doc->createElement("a:noFill");
+
+        $txBody = $doc->createElement("p:txBody");
+
+        $bodyPr = $doc->createElement("a:bodyPr");
+        $bodyPr->setAttribute("wrap", "square");
+        $bodyPr->setAttribute("rtlCol", "0");
+
+        $spAutoFit = $doc->createElement("a:spAutoFit");
+
+        $lstStyle = $doc->createElement("a:lstStyle");
+
+        $p = $doc->createElement("a:p");
+
+        $r = $doc->createElement("a:r");
+
+        $rPr = $doc->createElement("a:rPr");
+        $rPr->setAttribute("lang", "en-GB");
+        $rPr->setAttribute("sz", "1000");
+        $rPr->setAttribute("dirty", "0");
+        $rPr->setAttribute("smtClean", "0");
+
+        $t = $doc->createElement("a:t");
+        $t->nodeValue = $caption;
+
+        $endParaRPr = $doc->createElement("a:endParaRPr");
+        $endParaRPr->setAttribute('lang', "en-GB");
+        $endParaRPr->setAttribute("sz", "1000");
+        $endParaRPr->setAttribute("dirty", "0");
+
+        //nvSpPr
+        $nvSpPr->appendChild($cNvPr);
+        $nvSpPr->appendChild($cNvSpPr);
+        $nvSpPr->appendChild($nvPr);
+
+        //spPr
+        $xfrm->appendChild($off);
+        $xfrm->appendChild($ext);
+
+        $prstGeom->appendChild($avLst);
+
+        $spPr->appendChild($xfrm);
+        $spPr->appendChild($prstGeom);
+        $spPr->appendChild($noFill);
+
+        //txBody
+        $bodyPr->appendChild($spAutoFit);
+
+        $r->appendChild($rPr);
+        $r->appendChild($t);
+
+        $p->appendChild($r);
+        $p->appendChild($endParaRPr);
+
+        $txBody->appendChild($bodyPr);
+        $txBody->appendChild($lstStyle);
+        $txBody->appendChild($p);
+
+        $sp->appendChild($nvSpPr);
+        $sp->appendChild($spPr);
+        $sp->appendChild($txBody);
+        
+        return $sp;
+    }
 }
 
 class WordWriter extends OpenXmlWriter implements DocumentWriter
@@ -183,5 +362,8 @@ class WordWriter extends OpenXmlWriter implements DocumentWriter
         
     }   
 }
+
+$writer = new PowerPointWriter('bimdgfur4gefkdturqm8uvo5m2/test.pptx');
+//$reader->readWord();
 
 ?>
