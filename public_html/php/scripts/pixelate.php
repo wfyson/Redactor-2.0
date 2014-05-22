d707615db50a8da270dbe64413f7265a01ed0334
rwf1v07 <rwf1v07@UOS-205394.soton.ac.uk>
2014-05-22 15:32:27 +0100

    Change pixelate function to work with older versions of PHP

--- public_html/php/scripts/pixelate.php
+++ public_html/php/scripts/pixelate.php
@@ -12,11 +12,11 @@ $split = explode('.', $image);
 $format = strtolower($split[1]);
 
 //get references to the old image and a place for the new one
-$oldPath = '../../' . $image;  
-$newPath = '../../' . $split[0] . '_new.' . $split[1]; 
-    
+$oldPath = '../../' . $image;
+$newPath = '../../' . $split[0] . '_new.' . $split[1];
+
 //create an image reader depending on format
-switch ($format){
+switch ($format) {
     case "jpeg":
         $img = imagecreatefromjpeg($oldPath);
         break;
@@ -29,23 +29,38 @@ switch ($format){
 }
 
 //pixelate the image
-if($img && imagefilter($img, IMG_FILTER_PIXELATE, 20, true))
-{
-    //write the file back again
-    switch ($format)
-    {
-        case "jpeg":
-            $img = imagejpeg($img, $newPath);
-            break;
-        case "jpg":
-            $img = imagejpeg($img, $newPath);
-            break;
-        case "png":
-            $img = imagepng($img, $newPath);
-            break;
-    }    
+$size = getimagesize($oldPath);
+$height = $size[1];
+$width = $size[0];
+$pixelate_x = 40;
+$pixelate_y = 40;
+// start from the top-left pixel and keep looping until we have the desired effect
+for ($y = 0; $y < $height; $y += $pixelate_y + 1) {
+
+    for ($x = 0; $x < $width; $x += $pixelate_x + 1) {
+        // get the color for current pixel
+        $rgb = imagecolorsforindex($img, imagecolorat($img, $x, $y));
+
+        // get the closest color from palette
+        $color = imagecolorclosest($img, $rgb['red'], $rgb['green'], $rgb['blue']);
+        imagefilledrectangle($img, $x, $y, $x + $pixelate_x, $y + $pixelate_y, $color);
+    }
 }
 
-echo $_GET['callback'] . '(' . json_encode(substr($newPath, 6)) . ')';
 
+//write the file back again
+switch ($format) {
+    case "jpeg":
+        $img = imagejpeg($img, $newPath);
+        break;
+    case "jpg":
+        $img = imagejpeg($img, $newPath);
+        break;
+    case "png":
+        $img = imagepng($img, $newPath);
+        break;
+}
+
+
+echo $_GET['callback'] . '(' . json_encode(substr($newPath, 6)) . ')';
 ?>
